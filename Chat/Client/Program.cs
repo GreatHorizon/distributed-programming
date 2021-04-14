f04a315ace0b27914d42a6d31508c4494429cb05
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2021-04-14 21:18:20 +0300

    Added lab6

---
+++ Chat/Client/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Client
+{
+    class Program
+    {
+        public static void StartClient(string host, string port, string message)
+        {
+            try
+            {
+                IPAddress ipAddress;
+
+                if (host == "localhost")
+                {
+                    ipAddress = IPAddress.Loopback;
+                }
+                else
+                {
+                    ipAddress = IPAddress.Parse(host);
+                }
+
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, Convert.ToInt32(port));
+
+                Socket sender = new Socket(
+                    ipAddress.AddressFamily,
+                    SocketType.Stream, 
+                    ProtocolType.Tcp);
+
+                try
+                {
+                    sender.Connect(remoteEP);
+                    
+                    byte[] msg = Encoding.UTF8.GetBytes(message);
+                    int bytesSent = sender.Send(msg);
+
+                    byte[] buf = new byte[1024];
+                    int bytesRec = sender.Receive(buf);
+
+                    List<string> history = JsonSerializer.Deserialize<List<string>>(Encoding.UTF8.GetString(buf, 0, bytesRec));
+                    
+                    Console.WriteLine("History: ");
+                    foreach (string item in history)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                StartClient(args[0], args[1], args[2]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid arguments count");
+            }
+
+        }
+    }
+}
---
+++ Chat/Server/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Server
+{
+    class Program
+    {
+        public static void StartListening(string port)
+        {
+            IPAddress ipAddress = IPAddress.Any; 
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, Convert.ToInt32(port));
+            List<string> messageHistory = new List<string>();
+
+            // CREATE
+            Socket listener = new Socket(
+                ipAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connection...");
+                    Socket handler = listener.Accept();
+
+                    Console.WriteLine("Data receiving...");
+                    byte[] buf = new byte[1024];
+                    string data = null;
+                    int bytesRec = handler.Receive(buf);
+
+                    data = Encoding.UTF8.GetString(buf, 0, bytesRec);
+
+                    messageHistory.Add(data);
+
+                    Console.WriteLine("Received message: {0}", data);
+
+                    string historyJson = JsonSerializer.Serialize(messageHistory);
+                    byte[] history = Encoding.UTF8.GetBytes(historyJson);
+
+                    handler.Send(history);
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                StartListening(args[0]);
+
+                Console.WriteLine("\nPress enter to stop...");
+                Console.Read();    
+            }
+            else
+            {
+                Console.WriteLine("Invalid arguments count");
+            }
+        }
+    }
+}
